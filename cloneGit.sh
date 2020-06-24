59baf39ec9634d652532a8e32378f4aa7e96e815
nguyenvinhson <66408202+nguyenvinhson@users.noreply.github.com>
2020-06-24 15:47:21 +0700

    commit

---
+++ cloneGit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#----------------------------------------------------------------------------
+# name : cloneGit
+#
+# Description:
+#   メール送信機能を提供する。
+#
+#----------------------------------------------------------------------------
+
+repository="https://github.com/nguyenvinhson/demo-create-project.git"
+localFolder="C:\\Users\\nguyenvinhson\\Desktop\\Nhap"
+
+git clone "$repository $localFolder"
