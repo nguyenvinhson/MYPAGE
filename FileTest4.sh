bdb90eb2dc6f3775902d60f2e17081f4669ea934
nguyenvinhson <66408202+nguyenvinhson@users.noreply.github.com>
2020-07-03 11:36:01 +0700

    Update FileTest4.sh

--- FileTest4.sh
+++ FileTest4.sh
@@ -35,3 +35,32 @@ git add -A
 git commit -m 'test commit'
 git push -u origin master
 echo "Done push"
+cd $folder
+printf "Gitsource: "
+read -r gitsource
+git clone "$gitsource"
+unset gitsource
+echo "Done!"
+echo "*******************"
+foldercopy="$HOME"/mydir/copy
+if [ ! -d "$foldercopy" ]
+then
+	echo "create foldercopy"
+	mkdir $foldercopy
+fi
+echo "copy source git"
+cp -R $folder $foldercopy
+echo "Done copy!"
+resources="https://github.com/nguyenvinhson/demo-create-project.git"
+foldertemp=${resources##*/}
+project=${foldertemp%.*}
+echo "$project"
+cd $folder
+cd $project
+echo "create file"
+vi filename.sh
+git add filename.sh
+git add -A
+git commit -m 'test commit'
+git push -u origin master
+echo "Done push"
