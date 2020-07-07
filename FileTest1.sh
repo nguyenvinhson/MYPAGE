5e9016ad882ee922e348d9660922879797536840
nguyenvinhson <66408202+nguyenvinhson@users.noreply.github.com>
2020-07-07 14:15:36 +0700

    Update FileTest1.sh

--- FileTest1.sh
+++ FileTest1.sh
@@ -31,9 +31,3 @@ cp -R $folder $foldercopy
 echo "copy source git"
 cp -R $folder $foldercopy
 echo "Done copy!"
-resources="https://github.com/nguyenvinhson/demo-create-project.git"
-foldertemp=${resources##*/}
-project=${foldertemp%.*}
-echo "$project"
-echo "copy source git"
-cp -R $folder $foldercopy
